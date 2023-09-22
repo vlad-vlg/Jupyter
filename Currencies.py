5a65580f090c587369807c250aab2490b97409b4
Влад Иванович <vlad@ya.ru>
2023-09-22 14:23:32 +0300

    Добавление файлов Currencies

---
+++ Currencies.py
@@ -0,0 +1,16 @@
+
+import requests
+import xmltodict
+
+url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+response = requests.get(url)
+data = xmltodict.parse(response.content)
+print(data)
+
+my_array = []
+for item in data['ValCurs']['Valute']:
+    my_set = [item['CharCode'], item['Name'], item['Value']]
+    my_array.append(my_set)
+    print(my_set)
+
+# print(my_array)
