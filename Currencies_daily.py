2aeff6a59e93934aa8385fa2b7aad4b9c39ca8d3
Влад Иванович <vlad@ya.ru>
2023-09-22 15:29:31 +0300

    Добавление файлов Currencies_daily/ изменение файлов Currencies

--- Currencies_daily.py
+++ Currencies_daily.py
@@ -2,14 +2,14 @@
 import requests
 import xmltodict
 
-url = 'http://www.cbr.ru/scripts/XML_val.asp'
+url = 'http://www.cbr.ru/scripts/XML_daily.asp'
 response = requests.get(url)
 data = xmltodict.parse(response.content)
 print(data)
 
 my_array = []
-for item in data['Valuta']['Item']:
-    my_set = [item['Name'], item['EngName'], item['Nominal'], item['ParentCode']]
+for item in data['ValCurs']['Valute']:
+    my_set = [item['CharCode'], item['Nominal'], item['Name'], item['Value']]
     my_array.append(my_set)
     print(my_set)
 
